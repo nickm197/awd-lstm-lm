d5763169a93da967c272f7b109640c2b45cced87
nickm197 <nickm@ntrlab.com>
2023-01-10 16:05:08 +0300

    Cuda debug

--- data.py
+++ data.py
@@ -27,9 +27,9 @@ class Dictionary(object):
 class Corpus(object):
     def __init__(self, path):
         self.dictionary = Dictionary()
-        self.train = self.tokenize(os.path.join(path, 'train.txt'))
-        self.valid = self.tokenize(os.path.join(path, 'valid.txt'))
-        self.test = self.tokenize(os.path.join(path, 'test.txt'))
+        self.train = self.tokenize(os.path.join(path, 'ptb.train.txt'))
+        self.valid = self.tokenize(os.path.join(path, 'ptb.valid.txt'))
+        self.test = self.tokenize(os.path.join(path, 'ptb.test.txt'))
 
     def tokenize(self, path):
         """Tokenizes a text file."""
